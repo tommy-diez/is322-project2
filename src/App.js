c07b2a143b4b5e76a2aa02ad60a3dda050e01515
tommy-diez <td275@njit.edu>
2022-05-08 16:44:39 -0400

    Initial commit, set up API call

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            tasks: [],
+            errorMessage: ''
+        }
+    }
+
+    getData = () => {
+        axios({
+            method: 'get',
+            url:'http://my-json-server.typicode.com/tommy-diez/JSONDB/tasks',
+        }).then((response) => {
+            this.setState({tasks: response.data});
+            console.log(response.data);
+        }).catch((error) => {
+            this.setState({errorMessage: error.message})
+        })
+    }
+
+    componentDidMount() {
+        this.getData();
+    }
+
+    render() {
+        return (
+            <div>Task List </div>
+        )
+    }
+
 }
 
 export default App;
+
